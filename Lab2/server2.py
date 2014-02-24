240f675e9e35d7fd4bf9222fffb70472004a923f
martinbergdahl <bergdahl_martin@hotmail.com>
2014-02-25 00:04:29 +0100

    ee

--- Lab2/server2.py
+++ Lab2/server2.py
@@ -10,25 +10,19 @@ import database_helper2 as dh
 
 # create our little application :)
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['DEBUG'] = True
 
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route('/signin', methods=['POST', 'GET']) 
 def sign_in():
     email1 = request.args.get('email')
     password1 = request.args.get('password')
+    #password1 = create_hash(password1)
     length = 20
     generated_token = string.ascii_uppercase + string.digits +string.ascii_lowercase 
     token1 = ''.join(random.choice(generated_token) for i in range(length))
-    user = query_db('SELECT email,password FROM user_info WHERE email=? AND password=?',[email1,password1], one=True)
-    if user is None:
-        return 'No such user'
-    else:
-        db = get_db()
-        db.execute('UPDATE user_info SET token=? WHERE email=? AND password=? ' , [token1, email1, password1])
-        db.commit()
-        return token1
+    t = dh.sign_in(email1,password1,token1)
+    return t
 
 
 @app.route('/signup', methods=['POST'])
@@ -48,24 +42,26 @@ def change_password():
     token1 = request.args.get('token')
     oldPassword = request.args.get('oldPassword')
     newPassword = request.args.get('newPassword')
-    db = get_db()
-    db.execute('UPDATE user_info SET password=? WHERE token=?', [newPassword, token1])
-    db.commit()
-    return "You have now changed your password"
+    #oldPassword = create_hash(oldPassword)
+    #newPassword = create_hash(newPassword)
+    err= dh.change_password(token1, oldPassword, newPassword)
+    if err == "error":
+        return "You wrote wrong old password"
+    else:
+        return "You have now changed password"
    
 @app.route('/signout', methods=['POST'])
 def sign_out():
     token1 = request.args.get('token')
     tokreset = 'null'
-    db = get_db()
-    db.execute('UPDATE user_info SET token=? WHERE token1=?' , [tokreset, token1])
-    db.commit()
+    dh.sign_out(tokreset,token1)
     return "You have now signed-out"
+    
 
 @app.route('/getuserdatabytoken', methods=['POST', 'GET'])
 def get_user_data_by_token():
-    token1 = request.args.get('token')
-    user = query_db('SELECT email,firstname, familyname, gender, city, country FROM user_info WHERE token=?',[token1], one=True)
+    token = request.args.get('token')
+    user = dh.get_user_data_by_token(token)
     if user is None:
         return 'No such user'
     else:
@@ -74,45 +70,40 @@ def get_user_data_by_token():
 @app.route('/getuserdatabyemail', methods=['POST', 'GET'])
 def get_user_data_by_email():
     email = request.args.get('email') 
-    user = query_db('SELECT email,firstname, familyname, gender, city, country FROM user_info WHERE email=?',[email], one=True)
+    user = dh.get_user_data_by_email(email)
     if user is None:
         return 'No such user'
     else:
         return ",".join(user)
-
+    
 @app.route('/postmessage', methods=['POST'])
 def post_message():
     token = request.args.get('token')
     email = request.args.get('email')
     message = request.args.get('message')
-    au = query_db('SELECT email FROM user_info WHERE token=?',[token], one=True)
-    au = ",".join(au) 
-    db = get_db()
-    db.execute('insert into messanges(author, receiver, message) values (?,?,?)', [au, email, message])
-    db.commit()
+    dh.post_message(token, email, message)
     return "You have posted a message"
-    
+
 @app.route('/getusermessagesbytoken', methods=['POST', 'GET'])
 def get_user_messages_by_token():
     token = request.args.get('token')
-    re = query_db('SELECT email FROM user_info WHERE token=?',[token], one=True)
-    re = ",".join(re) 
-    mes = query_db('SELECT message FROM messanges WHERE receiver=?',[re], one=True)
+    mes = dh.get_user_messages_by_token(token)
     if mes is None:
         return 'No messages'
     else:
-        return ",".join(mes)
-
+        return ",".join(mes)   
+        
+        
 @app.route('/getusermessagesbyemail', methods=['POST', 'GET'])
 def get_user_messages_by_email():
     token = request.args.get('token')
     email = request.args.get('email')
-    mes = query_db('SELECT message FROM messanges WHERE receiver=?',[email], one=True)
+    mes = dh.get_user_messages_by_email(email)
     if mes is None:
         return 'No messages'
     else:
-        return ",".join(mes)
-    
+        return ",".join(mes)    
+
 
 if __name__ == '__main__':
     app.run()
