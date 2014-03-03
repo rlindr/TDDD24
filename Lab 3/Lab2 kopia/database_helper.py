5419f03629eda75d417f7357bb9cf43cc5933377
martinbergdahl <bergdahl_martin@hotmail.com>
2014-03-03 18:08:53 +0100

    Senast

---
+++ Lab 3/Lab2 kopia/database_helper.py
@@ -0,0 +1,149 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import sqlite3
+import os
+import random
+import string
+import sys
+import uuid
+import hashlib as ps
+ 
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'database.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == ps.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+
+def query_db(query, args=()):
+    cur = get_db().execute(query, args)
+    row = cur.fetchall()
+    cur.close()
+    return (row[0] if row else None)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('database.schema', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+def sign_in(email1,password1,token1):
+    user = query_db('SELECT email,password FROM user_info WHERE email=? AND password=?',[email1,password1])
+    if user == None:
+        return 'No such user registered in the database'
+    else:
+        db = get_db()
+        db.execute('UPDATE user_info SET token=? WHERE email=? AND password=? ' , [token1, email1, password1])
+        db.commit()
+        return token1
+
+
+def sign_up(firstname,familyname,gender,city,country,email,password):
+    user = query_db('SELECT email FROM user_info WHERE email=?',[email])
+    if user is None:
+        return 'There is already an user registered with that username'
+    else:
+        db = get_db()
+        db.execute('insert into user_info (firstname, familyname, gender, city, country, email, password) values (?,?,?,?,?,?,?)', [firstname, familyname, gender, city, country, email, password])
+        db.commit()
+
+def change_password(token1, oldPassword, newPassword):
+    
+    pc = query_db('SELECT password FROM user_info WHERE token=?',[token1])
+    if "".join(pc) ==  "".join(oldPassword):
+        db = get_db()
+        db.execute('UPDATE user_info SET password=? WHERE token=?', [newPassword, token1])
+        db.commit()
+    else:    
+        return "error"
+   
+
+def sign_out(tokreset,token1):
+    re = query_db('SELECT token FROM user_info WHERE token=?',[token1])
+    re = ",".join(re) 
+    if re == token1:
+        db = get_db()
+        db.execute('UPDATE user_info SET token=? WHERE token=?' , [tokreset, token1])
+        db.commit()
+    else:
+        return "You tried to log-out from wrong user"
+
+
+def get_user_data_by_token(token1):
+    user = query_db('SELECT email,firstname, familyname, gender, city, country FROM user_info WHERE token=?',[token1])
+    if user is None:
+        return 'None'
+    else:
+        return user
+
+def get_user_data_by_email(email):
+    user = query_db('SELECT email,firstname, familyname, gender, city, country FROM user_info WHERE email=?',[email])
+    if user is None:
+        return 'None'
+    else:
+        return user
+
+def post_message(token, email, message):
+    user = query_db('SELECT email FROM user_info WHERE email=?',[email])
+    if user is None:
+        return 'The reciever of this message is not registered'
+    else:    
+        au = query_db('SELECT email FROM user_info WHERE token=?',[token])
+        au = ",".join(au) 
+        db = get_db()
+        db.execute('insert into messanges(author, receiver, message) values (?,?,?)', [au, email, message])
+        db.commit()
+    
+def get_user_messages_by_token(token):
+    re = query_db('SELECT email FROM user_info WHERE token=?',[token])
+    re = ",".join(re) 
+    mes = query_db('SELECT message FROM messanges WHERE receiver=?',[re])
+    if mes is None:
+        return 'None'
+    else:
+        return mes
+        
+    
+def get_user_messages_by_email(email):
+    mes = query_db('SELECT message FROM messanges WHERE receiver=?',[email])
+    if mes is None:
+        return 'None'
+    else:
+        return mes
+ 
--- Lab 3/Lab2 kopia/server.py
+++ Lab 3/Lab2 kopia/server.py
@@ -5,30 +5,28 @@ import os
 import random
 import string
 import sys
-import database_helper2 as dh
+import database_helper as dh
 import uuid
 import hashlib as ps
  
-
-# create our little application :)
-app = Flask(__name__)
-app.config['DEBUG'] = True
-
-#The function that create the hash-password
 def create_hash(password):
     return ps.sha256(password.encode()).hexdigest()
+    
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
 
 @app.route('/signin', methods=['POST', 'GET']) 
 def sign_in():
     email1 = request.args.get('email')
     password1 = request.args.get('password')
-    password1 = create_hash(password1)
+    #password1 = create_hash(password1)
     length = 20
     generated_token = string.ascii_uppercase + string.digits +string.ascii_lowercase 
     token1 = ''.join(random.choice(generated_token) for i in range(length))
     t = dh.sign_in(email1,password1,token1)
     return t
-
 
 @app.route('/signup', methods=['POST'])
 def sign_up():
@@ -39,8 +37,8 @@ def sign_up():
     country = request.args.get('country')
     email = request.args.get('email')
     password = request.args.get('password')
-    password = create_hash(password)
-    dh.sign_up(firstname,familyname,gender,city, country, email, password)
+    #password = create_hash(password)
+    dh.sign_up(firstname,familyname,gender,city,country,email,password)
     return "You are now signed-up"
    
 @app.route('/changepassword', methods=['POST'])
@@ -48,13 +46,14 @@ def change_password():
     token1 = request.args.get('token')
     oldPassword = request.args.get('oldPassword')
     newPassword = request.args.get('newPassword')
-    oldPassword = create_hash(oldPassword)
-    newPassword = create_hash(newPassword)
+    #oldPassword = create_hash(oldPassword)
+    #newPassword = create_hash(newPassword)
     err= dh.change_password(token1, oldPassword, newPassword)
     if err == "error":
         return "You wrote wrong old password"
     else:
         return "You have now changed password"
+   
    
 @app.route('/signout', methods=['POST'])
 def sign_out():
@@ -62,13 +61,12 @@ def sign_out():
     tokreset = 'null'
     dh.sign_out(tokreset,token1)
     return "You have now signed-out"
-    
 
 @app.route('/getuserdatabytoken', methods=['POST', 'GET'])
 def get_user_data_by_token():
-    token = request.args.get('token')
-    user = dh.get_user_data_by_token(token)
-    if user is None:
+    token1 = request.args.get('token')
+    user = dh.get_user_data_by_token(token1)
+    if user == "None":
         return 'No such user'
     else:
         return ",".join(user)
@@ -77,11 +75,11 @@ def get_user_data_by_token():
 def get_user_data_by_email():
     email = request.args.get('email') 
     user = dh.get_user_data_by_email(email)
-    if user is None:
+    if user == "None":
         return 'No such user'
     else:
         return ",".join(user)
-    
+
 @app.route('/postmessage', methods=['POST'])
 def post_message():
     token = request.args.get('token')
@@ -89,27 +87,30 @@ def post_message():
     message = request.args.get('message')
     dh.post_message(token, email, message)
     return "You have posted a message"
-
+    
 @app.route('/getusermessagesbytoken', methods=['POST', 'GET'])
 def get_user_messages_by_token():
     token = request.args.get('token')
     mes = dh.get_user_messages_by_token(token)
-    if mes is None:
+    if mes == 'None':
         return 'No messages'
     else:
-        return mes    
+        return "".join(mes)
         
-        
+    
+
 @app.route('/getusermessagesbyemail', methods=['POST', 'GET'])
 def get_user_messages_by_email():
     token = request.args.get('token')
     email = request.args.get('email')
-    mes = dh.get_user_messages_by_email(email)
-    if mes is None:
+    mes = dh.get_user_data_by_email(email)
+    if mes == 'None':
         return 'No messages'
     else:
-        return mes  
-
+        return "".join(mes)
 
 if __name__ == '__main__':
     app.run()
+    
+    
+    
